8c5a2b0754dfcdc960281585fbd07db2aba42ebd
ANURAG SINGH <103880648+devilanurag@users.noreply.github.com>
2022-10-11 11:21:24 +0530

    Add files via upload

---
+++ Anu1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Anu1
+{
+public static void main(String a[])
+{
+Scanner sc=new Scanner(System.in);
+int x;
+System.out.println("enter the value of x");
+x=sc.nextInt();
+System.out.println(" value of x="+(x));
+}
+}
+
+
