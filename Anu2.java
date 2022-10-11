2d6c2f6972daeeb8a23b1985c28906e94f83b795
ANURAG SINGH <103880648+devilanurag@users.noreply.github.com>
2022-10-11 11:22:19 +0530

    Add files via upload

---
+++ Anu2.java
@@ -0,0 +1,16 @@
+//wap to take input from user and find their sum
+
+import java.util.Scanner;
+class Anu2
+{
+public static void main(String a[])
+{
+Scanner sc=new Scanner(System.in);
+int x,y;
+System.out.println("enter the value of x");
+x=sc.nextInt();
+System.out.println("enter the value of y");
+y=sc.nextInt();
+System.out.println(" sum of two nos.="+(x+y));
+}
+}
